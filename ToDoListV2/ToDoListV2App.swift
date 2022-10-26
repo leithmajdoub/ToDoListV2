1dee607760ed58ff6a8dfb7c241c22bbfe8b2849
leithmajdoub <majdoub.leith@gmail.com>
2022-10-26 10:28:05 +0100

    Initial Commit

---
+++ ToDoListV2/ToDoListV2App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListV2App.swift
+//  ToDoListV2
+//
+//  Created by Leith on 26/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
